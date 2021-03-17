2ad481ad8f0e6c9bd54761b7c4dca8c5c6954121
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2021-03-17 21:30:55 +0200

    Robot3 Arm functionality
    Robot3 arms grab target on specific given points.
    Robot3 arms can grab and hold objects that rotate and move.
    Fixed: arms target correctly the closest target to the robot's face an ton to detection collider.

---
+++ Robot Escape/Assets/Scripts/Interactable/Handle.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handle : MonoBehaviour
+{
+    public Transform pointsLeft;
+    public Transform pointsRight;
+}
--- Robot Escape/Assets/Scripts/Robots/R3/ArmsToPlayer.cs
+++ Robot Escape/Assets/Scripts/Robots/R3/ArmsToPlayer.cs
@@ -7,29 +7,52 @@ public class ArmsToPlayer : MonoBehaviour
     public bool activated = true;
     public Transform target;
     public float targetImportance;
-    //public LookAtPlayer lookAtPlayer;
     public Transform armLeft, armRight;
+    public Transform handLeft, handRight;
     public Transform leftTargetPoint, rightTargetPoint; 
     public Transform armLeftDefaultTarget, armRightDefaultTarget;
+    public Transform face;
     public float rotationSpeed = 5f;
+    public float grabHandleDistande = 0.4f;
+    public float grabSpeed = 5;
     public bool grab;
+    Quaternion handLeftDefaultRotation, handRightDefaultRotation;
     float armLenght;
     Vector3 leftTargetPointStart, rightTargetPointStart;
+    Handle leftHandle, rightHandle;
 
     void Start(){
+        leftHandle = null;
+        rightHandle = null;
         armLenght = Vector3.Distance(armLeft.position, leftTargetPoint.position);
         if (leftTargetPoint) leftTargetPointStart = leftTargetPoint.localPosition;
         if (rightTargetPoint) rightTargetPointStart = rightTargetPoint.localPosition;
+        if (handLeft) handLeftDefaultRotation = handLeft.rotation;
+        if (handRight) handRightDefaultRotation = handRight.rotation;
     }   
 
     void FixedUpdate(){
-        //if (lookAtPlayer == null) return;
-
         Transform targetL;
         Transform targetR;
+        Handle handle = null;
         if (target != null && grab && targetImportance>0){
-            targetL = target;
-            targetR = target;
+            handle = target.GetComponent<Handle>();
+            if (handle != null){
+                targetL = handle.pointsLeft != null ? handle.pointsLeft : target;
+                if (handle.pointsRight != null)
+                    targetR = handle.pointsRight;
+                else targetR = handle.pointsLeft != null ? handle.pointsLeft : target;
+                float dot = Vector3.Dot(transform.right, target.right);
+                if (dot < 0){
+                    Transform temp = targetL;
+                    targetL = targetR;
+                    targetR = temp;
+                }
+            }
+            else {
+                targetL = target;
+                targetR = target;
+            }
         }
         else {
             targetL = armLeftDefaultTarget;
@@ -51,6 +74,35 @@ public class ArmsToPlayer : MonoBehaviour
             else {
                 leftTargetPoint.localPosition = leftTargetPointStart;
             }
+
+            bool handlegrab = false;
+            Quaternion newRot = handLeftDefaultRotation;
+            if (handle != null){
+                if (handle.pointsLeft != null){
+                    float handleDist = Vector3.Distance(handLeft.position, targetL.position);
+                    if (handleDist <= grabHandleDistande){
+                        handlegrab = true;
+                        newRot = targetL.rotation;
+                        //handLeft.rotation = Quaternion.Slerp(handLeft.rotation, handle.pointsLeft.localRotation, grabSpeed * Time.fixedDeltaTime);
+                    }
+                }
+            }
+            if (handlegrab){
+                handLeft.rotation = Quaternion.Slerp(handLeft.rotation, newRot, grabSpeed * Time.fixedDeltaTime);
+            }
+            else {
+                handLeft.localRotation = Quaternion.Slerp(handLeft.localRotation, newRot, grabSpeed * Time.fixedDeltaTime);
+            }
+            
+            /*
+            if (handle != null){
+                if (handle.pointsLeft != null){
+                    Vector3 dirHandleL = (handle.pointsLeft.position - leftTargetPoint.position).normalized;
+                    Quaternion rotHandleL = Quaternion.Slerp(leftTargetPoint.rotation, Quaternion.LookRotation(dirHandleL), Time.fixedDeltaTime * rotationSpeed);
+                    leftTargetPoint.rotation = rotHandleL;
+                }
+            }
+            */
         }
 
         if (armRight){
@@ -64,6 +116,24 @@ public class ArmsToPlayer : MonoBehaviour
                 rightTargetPoint.localPosition = rightTargetPointStart;
             }
             
+            bool handlegrab = false;
+            Quaternion newRot = handRightDefaultRotation;
+            if (handle != null){
+                if (handle.pointsRight != null){
+                    float handleDist = Vector3.Distance(handRight.position, targetR.position);
+                    if (handleDist <= grabHandleDistande){
+                        handlegrab = true;
+                        newRot = targetR.rotation;
+                        //handRot = Quaternion.Slerp(handRight.rotation, handle.pointsRight.localRotation, grabSpeed * Time.fixedDeltaTime);
+                    }
+                }
+            }
+            if (handlegrab){
+                handRight.rotation = Quaternion.Slerp(handRight.rotation, newRot, grabSpeed * Time.fixedDeltaTime);
+            }
+            else {
+                handRight.localRotation = Quaternion.Slerp(handRight.localRotation, newRot, grabSpeed * Time.fixedDeltaTime);
+            }
         }
         //Vector3 dirL = (targetL.position - armLeft.position).normalized;
         //Vector3 dirR = (targetR.position - armRight.position).normalized;
@@ -101,7 +171,7 @@ public class ArmsToPlayer : MonoBehaviour
                 target = trigger;
                 targetImportance = importance;
             }
-            else if (importance == targetImportance && Vector3.Distance(transform.position, trigger.position) < Vector3.Distance(transform.position, target.position)){
+            else if (importance == targetImportance && Vector3.Distance(face.position, trigger.position) < Vector3.Distance(face.position, target.position)){
                 target = trigger;
                 targetImportance = importance;
             }
