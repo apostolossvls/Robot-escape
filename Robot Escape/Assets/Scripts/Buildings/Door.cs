ffc5fbfbdf076701fb1f3091ca43229a0e0cbdf0
apostolossvls <35051397+apostolossvls@users.noreply.github.com>
2021-04-13 01:22:22 +0300

    Added Door script

---
+++ Robot Escape/Assets/Scripts/Buildings/Door.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    protected bool IsOpen = false;
+
+    public bool GetIsOpen(){
+        return IsOpen;
+    }
+}
